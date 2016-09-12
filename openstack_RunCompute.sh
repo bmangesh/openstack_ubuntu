a4c7330630a503350dbf3b1f6937c36ab050dd28
root <mangeshsoft0@gmail.com>
2016-09-13 02:58:54 +0530

    copy from current directory

--- openstack_RunCompute.sh
+++ openstack_RunCompute.sh
@@ -5,7 +5,7 @@ scp /root/requirment.sh $Comp_IP:/root
 
 scp /etc/hosts $Comp_IP:/etc/hosts
 
-scp /root/openstack_ubuntu/openstack_ComputeNode.sh $Comp_IP:/root/openstack_ComputeNode.sh
+scp ./openstack_ComputeNode.sh $Comp_IP:/root/openstack_ComputeNode.sh
 
 ssh $Comp_IP "sh /root/openstack_ComputeNode.sh" 
 
