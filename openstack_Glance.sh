f240452e6461a6ed3bd199738e777dcff35517e8
root <mangeshsoft0@gmail.com>
2016-09-13 00:10:57 +0530

    Fix image upload issue

--- openstack_Glance.sh
+++ openstack_Glance.sh
@@ -100,4 +100,7 @@ echo "export OS_IMAGE_API_VERSION=2" | tee -a /root/admin-openrc.sh
 mkdir /tmp/images
 wget -P /tmp/images http://download.cirros-cloud.net/0.3.4/cirros-0.3.4-x86_64-disk.img
 
-glance image-create --name "cirros-0.3.4-x86_64" --file /tmp/images/cirros-0.3.4-x86_64-disk.img --disk-format qcow2 --container-format bare --visibility public --progress
+glance image-list
+glance image-create --name "cirros-0.3.4-x86_64" --file /tmp/images/cirros-0.3.4-x86_64-disk.img   --disk-format qcow2 --container-format bare --visibility public --progress
+
+glance image-list
