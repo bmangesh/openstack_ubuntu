d1dfc891544152121a38c4f349f06deea5238891
Mangesh B Bharsakle <mangeshsoft0@gmail.com>
2016-09-14 01:35:58 +0530

    Fixed Source issue

--- openstack_horizon.sh
+++ openstack_horizon.sh
@@ -1,5 +1,4 @@
 #! /bin/bash
-
 # Author : Mangeshkumar B Bharsakle
 
 #Install Horizon Package
@@ -7,8 +6,6 @@
 apt-get install openstack-dashboard -y
 
 sed -i "s/OPENSTACK_HOST = \"127.0.0.1\"/OPENSTACK_HOST = \"controller\"/g" /etc/openstack-dashboard/local_settings.py
-
-
 
 sed -i "s/OPENSTACK_KEYSTONE_DEFAULT_ROLE = \"_member_\"/OPENSTACK_KEYSTONE_DEFAULT_ROLE = \"user\"/g" /etc/openstack-dashboard/local_settings.py
 
