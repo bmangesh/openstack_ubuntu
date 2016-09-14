ccaebb6b9db4adb52eec1712ebf186714b535e24
mangeshsoft0@gmail.com <mangeshsoft0@gmail.com>
2016-09-15 02:41:00 +0530

    added admin vnet

--- setup_Vnetwork.sh
+++ setup_Vnetwork.sh
@@ -1,15 +1,18 @@
+#! /bin/bash
+
+
 neutron net-create ext-net --router:external   --provider:physical_network external --provider:network_type flat
 
 neutron subnet-create ext-net 192.168.2.0/24  --name ext-subnet --allocation-pool start=192.168.2.170,end=192.168.2.180 --disable-dhcp --gateway  192.168.2.1
 
-neutron net-create demo-net
+neutron net-create admin-net
 
-neutron subnet-create demo-net 10.10.10.0/24 --name demo-subnet --dns-nameserver 8.8.4.4 --gateway 10.10.10.1
+neutron subnet-create admin-net 10.10.10.0/24 --name admin-subnet --dns-nameserver 8.8.4.4 --gateway 10.10.10.1
 
-neutron router-create demo-router
+neutron router-create admin-router
 
-neutron router-interface-add demo-router demo-subnet
+neutron router-interface-add admin-router admin-subnet
 
-neutron router-gateway-set demo-router ext-net
+neutron router-gateway-set admin-router ext-net
 
 
